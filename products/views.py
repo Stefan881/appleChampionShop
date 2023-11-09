449abcb4af60019ccd4dea7e9cda2a99f51f934a
Stefan881 <ondeyostephen0@gmail.com>
2023-11-09 17:01:58 +0300

    Displayed the added products on on the template

--- products/views.py
+++ products/views.py
@@ -1,16 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from django.contrib import messages
+from .models import Product
 
 
 # Create your views here.
 def products(request):
-    return render(request, 'products/products.html')
+    all_products = Product.objects.all()
+    context = {"products": all_products}
+    return render(request, 'products/products.html',context)
 
 
 def add_products(request):
-    return render(request, 'products/addProducts.html')
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product saved successfully')
+            return redirect('add-products-url')
+        else:
+            messages.error(request, 'Product saving failed')
+            return redirect('add-products-url')
+    else:
+        form = ProductForm()
+    return render(request, 'products/addProducts.html', {"form": form})
 
 
 def update_products(request):
     return render(request, 'products/updateProducts.html')
-
-
