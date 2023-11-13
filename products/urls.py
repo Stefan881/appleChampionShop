fe23a271bd3975913b6276566f18572a81052889
Stefan881 <ondeyostephen0@gmail.com>
2023-11-13 12:26:15 +0300

    Added delete,update and pay functionalities

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,7 @@ from . import views as stephen
 urlpatterns = [
     path('', stephen.products, name='products-url'),
     path('add-products/', stephen.add_products, name='add-products-url'),
+    path('delete/<id>', stephen.delete, name='delete-url'),
+    path('update/<id>', stephen.update_products, name='update-url'),
+    path('pay/<id>', stephen.pay, name="pay-url"),
 ]
